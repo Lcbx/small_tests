9a0a02fbbe8b049422a3f55c7ba40027116b7430
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2019-02-02 13:06:52 -0500

    log(n) complexity insertion in Organiser

--- prioritySequence.py
+++ prioritySequence.py
@@ -1,45 +1,81 @@
+import collections
+
+Entry = collections.namedtuple('Entry', 'time priority item')
 
 class Organiser :
 	
 	def __init__(self):
 		self.heap = []
+		self.time = 0.
 	
-	def push(self, basePriority, element):
-		self.heap.append( (1./basePriority, 1./basePriority, element) )
+	# linear complexity push algorithm
+	def _push(self, priority, item):
+		current = Entry(self.time + 1./priority, priority, item)
+		for i, entry in enumerate(self.heap):
+			if current.time > entry.time:
+				self.heap.insert(i, entry)
+				return
+		self.heap.append(current)
+	
+	# log(n) complexity push algorithm
+	def push(self, priority, item):
+		entry = Entry(self.time + 1./priority, priority, item)
+		if len(self.heap) == 0:
+			self.heap.append(entry)
+		else:
+			self.heap.insert(self.findPlace(entry), entry)
+	
+	# divide and conquer algorithm, for kicks
+	def findPlace(self, entry):
+		return self._findplace(entry, 0, len(self.heap))
+		
+	def _findplace(self, entry, start, end):
+		space = end - start
+		if space <= 1:
+			if entry.time > self.heap[start].time:
+				return start
+			else:
+				return end
+			
+		median = start + space//2
+		if entry.time > self.heap[median].time:
+			return self._findplace(entry, start, median)
+		else:
+			return self._findplace(entry, median, end)
+			
 	
 	def pop(self):
-		self.heap.sort(key = lambda l: (l[0], l[1]), reverse = True)
 		current = self.heap.pop()
+		self.time = current.time
 		return current
+
+		
+if __name__ == "__main__":
+
+	size = 20000
+			
+	o = Organiser()
 	
-	def advance(self):
-		current = self.pop()
-		self.heap.append( (current[0] + current[1], current[1], current[2]) )
-		return current[2]
-		
-size = 500
-		
-o = Organiser()
+	priorities = [*range(1, 500)] 
+	for number in priorities:
+		o.push(number, number)
+	
+	frequencies = [0] * len(priorities)
+	for i in range(size):
+		current = o.pop()
+		o.push(current.priority, current.item)
+		if i < 20:
+			print(str(current.item) + ", ", end = "")
+		frequencies[current.item-1] +=1
+	print()
+	print(frequencies)
 
-priorities = [23., 25., 27., 30., 38.]
-for i in xrange(len(priorities)):
-	o.push(priorities[i],i)
+	frequencies = list(priorities)
+	tot = 0
+	for i in frequencies:
+		tot +=i
+	for i in range(len(frequencies)):
+		frequencies[i] *= size/tot
+	print(frequencies)
 
-frequencies = [0,0,0,0,0]
-for i in xrange(size):
-	current = o.advance()
-	if i > size - 20:
-		print str(current) + ", ",
-	frequencies[current] +=1
-print
-print frequencies
-
-frequencies = list(priorities)
-tot = 0
-for i in frequencies:
-	tot +=i
-for i in xrange(len(frequencies)):
-	frequencies[i] *= size/tot
-print frequencies
-
-# interesting :P
+	# interesting :P
