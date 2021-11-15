fefe39a28bc0c99316f389449241f40709b8f61d
lcourbi <luc.courbariaux@gmail.com>
2021-11-16 00:05:07 +0100

    algo still worse in cpp

--- cpp files/Hierarchy.cpp
+++ cpp files/Hierarchy.cpp
@@ -3,7 +3,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <math.h>
 #include <cassert>
+
+#include <chrono>
+#include <utility>
+
+typedef std::chrono::high_resolution_clock::time_point TimeVar;
+
+#define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
+#define timeNow() std::chrono::high_resolution_clock::now()
+
+template<typename F, typename... Args>
+double funcTime(F func, Args&&... args) {
+	TimeVar t1 = timeNow();
+	func(std::forward<Args>(args)...);
+	return duration(timeNow() - t1);
+}
+
+#undef duration
+#undef timeNow
+
 
 
 class AbstractNode {
@@ -48,7 +69,7 @@ public:
 		return std::string(7 - name.length(), '-') + name;
 	}
 
-	void printTree(int level = 0){
+	std::string printTree(int level = 0){
 
 		auto repr = prettyName(Name);
 		if (level > 0) {
@@ -65,29 +86,32 @@ public:
 			current = &firstChild;
 		}
 		
-		std::cout << repr << std::endl;
+		repr += "\n";
 
 		while (exploredLevel!=level) {
 			current = &current->getParent();
 			exploredLevel -= 1;
-			current->_endPrintTree(exploredLevel);
+			repr += current->_endPrintTree(exploredLevel);
 		}
+		return repr;
 	}
-	void _endPrintTree(int level) {
+	std::string _endPrintTree(int level) {
+		std::string repr = "";
 		auto children = getChildren();
 		auto it = children.begin();
 		it++;
 		while (it != children.end())
 		{
-			(*it)->printTree(level +1);
+			repr += (*it)->printTree(level +1);
 			it++;
 		}
+		return repr;
 	}
 };
 
 
 class NaiveNode : public AbstractNode {
-	AbstractNode* Parent;
+	AbstractNode* Parent = nullptr;
 	std::vector<AbstractNode*> Children = std::vector<AbstractNode*>();
 public:
 
@@ -122,6 +146,72 @@ public:
 	}
 };
 
+
+class MyNode : public AbstractNode {
+	static std::map<int, MyNode*> Tree;
+	int Hash = -1;
+	int nChildren = 0;
+public:
+
+	virtual MyNode& getParent() {
+		return *Tree[(Hash - 1) / 2];
+	}
+
+	virtual void setParent(MyNode& parent) {
+		// Tree = parent.Tree;
+		if (&parent == this) {
+			Hash = 0;
+		}
+		else if (!parent.hasChildren()) {
+			Hash = parent.Hash * 2 + 1;
+			parent.nChildren += 1;
+		}
+		else {
+			int n = parent.nChildren;
+			Hash = (parent.Hash+1) * pow(2, n+1) + pow(2, n) -2;
+			parent.nChildren += 1;
+		}
+		Tree[Hash] = this;
+	}
+
+	virtual void addChild(MyNode& child) {
+		child.setParent(*this);
+	}
+
+	virtual std::vector<AbstractNode*> getChildren() {
+		if (hasChildren()) {
+			auto result = std::vector<AbstractNode*>();
+			MyNode* current = &getFirstChild();
+			result.push_back(current);
+			while (current->hasSibling()){
+				auto sibling = &current->getSibling();
+				result.push_back(sibling);
+				current = sibling;
+			}
+			return result;
+		}
+		return std::vector<AbstractNode*>();
+	}
+
+	virtual bool hasChildren() {
+		return nChildren != 0;
+	}
+
+	virtual MyNode& getFirstChild() {
+		return *Tree[Hash * 2 + 1];
+	}
+
+	virtual bool hasSibling() {
+		return Tree.count(Hash * 2 + 2);
+	}
+
+	virtual MyNode& getSibling() {
+		return *Tree[Hash * 2 + 2];
+	}
+};
+std::map<int, MyNode*> MyNode::Tree = std::map<int, MyNode*>();
+
+
 template<typename T>
 std::vector<T> generateTree(int size) {
 	std::srand(42);
@@ -151,10 +241,29 @@ std::vector<T> generateTree(int size) {
 	return vector;
 }
 
+template<typename T>
+std::vector<T> testTree(int size) {
+	std::srand(113);
+	
+
+}
+
+
 int main() {
 
-	auto naive = generateTree<NaiveNode>(10);
-	naive[0].printTree();
+	static auto naive = generateTree<NaiveNode>(1000);
+	//std::cout << naive[0].printTree();
+
+	static auto mine = generateTree<MyNode>(1000);
+	//std::cout << mine[0].printTree();
+
+	auto testN = [] { naive[0].printTree(); };
+	auto testM = [] { mine[0].printTree(); };
+
+
+	std::cout << "norm: " << funcTime(testN) << "\n";
+	std::cout << "algo: " << funcTime(testM);
+
 
 	int i;
 	std::cin >> i;
